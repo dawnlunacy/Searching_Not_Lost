451c760620620bc817adff57d0663b8369b15271
dawnlunacy <rudd.lacy@gmail.com>
2020-01-14 15:31:15 -0700

    Add searchForm component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form>
+    <input 
+    className="searchInput"
+    type="text"
+    placeholder=" Input Keyword to Search"
+    name="search"/>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchForm',
+ }
+</script>
+
+<style scoped>
+  input {
+    height: 50px;
+    width: 500px;
+    margin-top: 50px;
+    border-radius: 10px;
+  }
+</style>
